d13e309f89ce3b851b92cbd010151f5cb6ea125b
tatsuya.yamamoto <tatsuya.yamamoto69@gmail.com>
2016-07-29 22:39:52 +0900

    adjust npm files.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,57 @@
 'use strict';
+/**
+ * フラグ用変数
+ * デフォルトで動作させる場合はtrueをセット
+ */
+let flg_php  = false;
+let flg_jade = false;
+let flg_min  = false;
+
+/**
+ * 環境設定
+ */
+const CONFIG = {
+  outputDirectory: {
+    dev : './src/',
+    release : './release/',
+  },
+  sourceDirectory: {
+    sass : './src/**/*.scss',
+    jsLib : './src/assets/js/libs/**/*.js',
+    js : './src/assets/js',
+    es6 : './src/**/*.es6',
+    jade : './src/**/*.jade',
+    jadeNone : './src/**/_*.jade',
+  },
+  watchDirectory: {
+    html : './src/**/*.html',
+    jade : './src/**/*.jade',
+    css : './src/**/*.css',
+    php : './src/**/*.php',
+    sass : './src/**/*.scss',
+  },
+  deployFile: [
+    './src/**',
+    '!./src/assets/js/libs/*.js',
+    '!./src/**/*.scss',
+    '!./src/**/*.jade',
+    '!./src/**/*.es6'
+  ],
+  cleanFile: [
+    './release/**/libs',
+  ]
+};
+const SASS_AUTOPREFIXER_BROWSERS = [
+  'ie >= 8',
+  'ios >= 7',
+  'android >= 2.3',
+  'last 4 versions'
+];
+const SASS_OUTPUT_STYLE = "expanded"; //nested, compact, compressed, expanded.
+
+/**
+ * IMPORT MODULES
+ */
 import gulp from 'gulp';
 import runSequence from 'run-sequence';
 import del from 'del';
@@ -8,39 +61,35 @@ import plumber from 'gulp-plumber';
 import pleeease from 'gulp-pleeease';
 import babel from 'gulp-babel';
 import concat from 'gulp-concat';
+import connectPhp from 'gulp-connect-php';
 import uglify from 'gulp-uglify';
+import header from 'gulp-header';
 import eslint from 'gulp-eslint';
+import htmlhint from 'gulp-htmlhint';
+import jade from 'gulp-jade';
 import gulpIf from 'gulp-if';
 import browserSync from 'browser-sync';
 import minimist from 'minimist';
-const reload = browserSync.reload;
-const DevDir = './src/',
-  ReleaseDir = './release/',
-  SassDir = './src/**/*.scss',
-  JsLibsDir = './src/assets/js/libs/**/*.js',
-  JsDir = './src/assets/js',
-  TypeDir = './src/**/*.ts',
-  Es6Dir = './src/**/*.es6',
-  HtmlWatchdir = './src/**/*.html',
-  CssWatchdir = './src/**/*.css',
-  PhpWatchdir = './src/**/*.php',
-  SassWatchdir = './src/**/*.scss';
+const RELOAD = browserSync.reload;
 
-//default task ->
+/**
+ * DEFAULT TASKS
+ */
 gulp.task('sass', () => {
-  gulp.src(SassDir)
+  gulp.src(CONFIG.sourceDirectory.sass)
   .pipe(plumber())
-  .pipe(sass({outputStyle: 'expanded'})) //nested, compact, compressed, expanded.
-  //.pipe(concat('concat.css'))
+  .pipe(sass({outputStyle: SASS_OUTPUT_STYLE}))
+  //.pipe(concat('min.css'))
   .pipe(pleeease({
-    autoprefixer: {'browsers': ['last 4 versions','ie 8', 'ios 7','Android 2.3']},
+    autoprefixer: {'browsers': SASS_AUTOPREFIXER_BROWSERS},
     minifier: false
   }))
-  .pipe(gulp.dest(DevDir))
+  //.pipe(header('@charset 'utf-8';\n'))
+  .pipe(gulp.dest(CONFIG.outputDirectory.dev))
   .pipe(browserSync.reload({stream:true}));
 });
 gulp.task('eslint', () => {
-  return gulp.src([Es6Dir])
+  return gulp.src([CONFIG.sourceDirectory.es6])
   .pipe(plumber())
   .pipe(eslint({
     globals: {
@@ -51,59 +100,110 @@ gulp.task('eslint', () => {
   .pipe(eslint.format())
 });
 gulp.task('babel',['eslint'], () => {
-  return gulp.src([Es6Dir])
+  return gulp.src([CONFIG.sourceDirectory.es6])
   .pipe(plumber())
   .pipe(babel())
-  //.pipe(concat('concat.js'))
-  //.pipe(uglify({preserveComments: 'some'}))
-  .pipe(gulp.dest(DevDir))
+  //.pipe(concat('min.js'))
+  .pipe(gulpIf( flg_min, uglify({preserveComments: 'some'}) ) )
+  .pipe(gulp.dest(CONFIG.outputDirectory.dev))
   .pipe(browserSync.reload({stream:true}));
 });
+gulp.task('htmllint', () => {
+  return gulp.src([CONFIG.watchDirectory.html])
+  .pipe(plumber())
+  .pipe(htmlhint('.htmlhintrc'))
+  .pipe(htmlhint.reporter())
+});
+gulp.task('jadehtml', function() {
+  return gulp.src([CONFIG.sourceDirectory.jade,'!'+CONFIG.sourceDirectory.jadeNone])
+  .pipe(plumber())
+  .pipe(jade({
+    pretty: true
+    //client: true //output to javascript
+  }))
+  .pipe(gulp.dest(CONFIG.outputDirectory.dev))
+});
 gulp.task('js', () => {
-  gulp.src([JsLibsDir])
+  gulp.src([CONFIG.sourceDirectory.jsLib])
   .pipe(plumber())
   .pipe(concat('libs.js'))
-  //.pipe(uglify({preserveComments: 'some'}))
-  .pipe(gulp.dest(JsDir))
+  .pipe(gulpIf( flg_min, uglify({preserveComments: 'some'}) ) )
+  .pipe(gulp.dest(CONFIG.sourceDirectory.js))
   .pipe(browserSync.reload({stream:true}));
 });
 gulp.task('reload', () => {
   gulp.src().pipe(browserSync.reload({stream:true}));
 });
 gulp.task('watch',['server'], () => {
-  gulp.watch(Es6Dir,['babel']);
-  gulp.watch(SassWatchdir,['sass']);
+  gulp.watch(CONFIG.watchDirectory.es6,['babel']);
+  gulp.watch(CONFIG.watchDirectory.sass,['sass']);
+  gulp.watch(CONFIG.watchDirectory.html,['htmllint']);
+  if(flg_jade){
+    gulp.watch(CONFIG.watchDirectory.jade,['jadehtml']);
+  }
 });
 gulp.task('server', () => {
-  browserSync({
-    server: {
-      baseDir: DevDir
-    }
-  });
-  gulp.watch(HtmlWatchdir, reload);
-  gulp.watch(PhpWatchdir, reload);
+  if(flg_php){
+    //Use php
+    connectPhp.server({
+      port: 8000,
+      base: CONFIG.outputDirectory.dev
+    }, function (){
+      browserSync({
+        proxy: 'localhost:8000'
+      });
+    });
+  } else {
+    //Use html only
+    browserSync({
+      server: {
+        baseDir: CONFIG.outputDirectory.dev,
+      },
+      notify: true,
+      logPrefix: 'browserSync',
+      // https: true,
+    })
+  };
+  gulp.watch(CONFIG.watchDirectory.html, RELOAD);
+  if(flg_php){
+    gulp.watch(CONFIG.watchDirectory.php, RELOAD);
+  }
 });
 gulp.task('default', (callback) => {
   let env = minimist(process.argv.slice(2));
-  console.log('$ gulp --[pc or sp] --[filename] // 拡張子無し');
-  console.log(env);
-  return runSequence(['sass','babel'],'js','watch',callback);
+  console.log('$ gulp --[pc or sp] --[php] --[jade] --[min]');
+  Object.keys(env).forEach(function (key) {
+    console.log("key -> " + key + "  value -> " + env[key]);
+    switch(key){
+      case 'php': flg_php = true;
+        break;
+      case 'jade': flg_jade = true;
+        break;
+      case 'min': flg_min = true;
+        break;
+    }
+  });
+  if(flg_jade) {
+    return runSequence(['sass','babel'],'js','jadehtml','watch',callback);
+  } else {
+    return runSequence(['sass','babel'],'js','watch',callback);
+  }
 });
-//default task
 
-//release task ->
+/**
+ * RELEASE TASKS
+ */
 gulp.task('deploy', () => {
-  gulp.src(['./src/**','!./src/assets/js/libs/*.js','!./src/**/*.scss','!./src/**/*.es6'],
-   {base:'src'})
-   .pipe(gulp.dest('release'))
+  return gulp.src(CONFIG.deployFile,{
+      base:CONFIG.outputDirectory.src
+    })
+     .pipe(gulp.dest(CONFIG.outputDirectory.release))
 });
-gulp.task('clean', (cb) => {
-  del(['./release/**/libs'], cb);
+gulp.task('clean', () => {
+  return del(CONFIG.cleanFile).then(paths => {
+      console.log('Deleted files and folders:\n', paths.join('\n'));
+    });
 });
 gulp.task('release', (callback) => {
-  let env = minimist(process.argv.slice(2));
-  console.log('$ gulp --[pc or sp] --[filename] // 拡張子無し');
-  console.log(env);
-  return runSequence(['sass','babel'],'js','deploy','clean',callback);
+  return runSequence(['sass','babel'],'js','jadehtml','deploy','clean',callback);
 });
-//release task
